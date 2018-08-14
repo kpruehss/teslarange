1abc3443006d63d34edcf300d9206dda6f3dcb4b
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-08-14 11:02:14 +1000

    ADD: Header + Logo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from 'components/layout/Header';
 
 class App extends Component {
   render() {
     return (
-      <h1>Tesla app</h1>
+      <Header />
     );
   }
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/Logo';
+
+export default () => {
+  return (
+    <header className="header">
+      <Logo />
+    </header>
+  );
+};
