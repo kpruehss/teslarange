0c0e3dc05de557550740d46c774124d1b9baeaed
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-08-17 10:54:50 +1000

    starting to implement context API for state management

---
+++ src/context.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import data from 'data.json';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {data};
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+
+export const Consumer = Context.Consumer;
